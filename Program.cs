368d7d1c2df0164789a24a36762a6478afd0abcc
BinhTran <fam.out16@gmail.com>
2024-08-09 20:14:52 +0700

    Add cac files cua bai thuc hanh tinh so ngay trong thang

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.InteropServices;
+namespace SUDUNGSWITCHCASE
+{
+    class DemSoNgayCuaThang
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap thang can xet:");
+            int thang = int.Parse(Console.ReadLine());
+            string songay = "";
+            switch (thang)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12: songay = "31"; break;
+                case 2: songay = "28 hoac 29"; break;
+                case 4:
+                case 6:
+                case 9:
+                case 11: songay = "30"; break;
+                default: Console.WriteLine("Thang khong hop le"); break;
+            }
+            if (thang<=12&&thang>=1)
+            {
+                Console.WriteLine("Thang {0} co {1} ngay", thang, songay);
+            }
+        }
+    }
+}
